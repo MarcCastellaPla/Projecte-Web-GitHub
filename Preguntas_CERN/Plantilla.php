1c32342fe108a6849d84d5be5c9954afa9adb903
MCP <marc.castellap@mataro.epiaedu.cat>
2024-04-02 17:58:14 +0200

    Completat PHP

--- Preguntas_CERN/Plantilla.php
+++ Preguntas_CERN/Plantilla.php
@@ -16,23 +16,47 @@
         // Fer un sistema per que envii quina era la pregunta anterior i comprovi si la resposta era correcte.
         // Fer aixo mateix per guardar una llista de les preguntes que ja han sortit, per tal de no repetir-les.
             $llista_preguntes = array(
-                "Elemento 1",
-                "Elemento 2",
-                "Elemento 3",
-                "Elemento 4",
-                "Elemento 5",
-                "Elemento 6",
-                "Elemento 7",
-                "Elemento 8",
-                "Elemento 9",
-                "Elemento 10"
+                "Com es diu l’accelerador de partícules més gran del CERN (i del món)",
+                "Que signifiquen les sigles CERN?",
+                "Quan va ser format el CERN?",
+                "Quina particula fundamental va descubrir el CERN?",
+                "A quin país està la seu principal del CERN?",
+                "Quin dels següents és el nom d’un dels detectors del LHC?",
+                "Quin és el propòsit principal del LHC?",
+                "On s’ubica el CERN?",
+                "Quins d’aquests experiments han sigut realitzats pel CERN?",
+                "Quins països són membres fundadors del CERN?"
             );
-            
-            $numaleatori = rand(0,9);
-            $pregunta = $llista_preguntes[$numaleatori];
+
             $numanterior = isset($_POST["numanterior"]) ? $_POST["numanterior"] : null;
+            $panteriors = isset($_POST["panteriors"]) ? $_POST["panteriors"] : null;
             $operador = isset($_POST["pregunta_cern"]) ? $_POST["pregunta_cern"] : null;
             echo "<h1>$numanterior</h1>";
+            
+            $numaleatori = rand(0, 9);
+            $lista = str_split($panteriors);
+            while (in_array($numaleatori, $lista)) {
+                $numaleatori = rand(0, 9); // Generar otro número aleatorio
+            }
+            $panteriors .= $numaleatori;
+            $lista = str_split($panteriors);
+            
+            
+            $pactual = $numaleatori;
+            $pregunta = $llista_preguntes[$numaleatori];
+            $completat = true;
+            for ($i = 0; $i <= 9; $i++) {
+                // Verificar si el número está presente en el array
+                if (!in_array($i, $lista)) {
+                    $completat = false;
+                }
+            }
+            if ($completat) {
+                $action = "../index.php";
+            } else {
+                $action = "Plantilla.php";
+            }
+            
             
             
 
@@ -76,18 +100,27 @@
                 $resposta2 = "B8";
                 $resposta3 = "C8";
                 $resposta4 = "D8";
-            } elseif ($numaleatori == 8) {
+            } elseif ($numaleatori == 8) { // Multiples respostes
                 $resposta1 = "A9";
                 $resposta2 = "B9";
                 $resposta3 = "C9";
                 $resposta4 = "D9";
-            } elseif ($numaleatori == 9) {
+            } elseif ($numaleatori == 9) { // Multiples respostes
                 $resposta1 = "A10";
                 $resposta2 = "B10";
                 $resposta3 = "C10";
                 $resposta4 = "D10";
             }
+            if ($numaleatori == 9 || $numaleatori == 8) {
+                $tipus = "checkbox";
+                $correcte2 = $resposta2;
+            } else {
+                $tipus = "radio";
+                $correcte2 = "false";
+            }
+            
             $correcte = $resposta1;
+            
             $pregunta = $llista_preguntes[$numaleatori];
             echo '<p class="pregunta" id="pregunta">'. $pregunta. '</p>';
         
@@ -97,25 +130,28 @@
         <img src=\"imatges/LogoCERN.png\" alt=\"CERN\" class=\"imatge\">
 
         <div class=\"respostes\">
-            <form action=\"Plantilla.php\" method=\"post\">
+            <form action=\"$action\" method=\"post\">
                 <div class=\"respostesadalt\">
                     <div class=\"resposta1\">
-                    <label for=\"pregunta_cern\"><input type=\"radio\" name=\"pregunta_cern\" id=\"resposta1\" id=\"resposta1\" value=\"$resposta1\">$correcte</label>
+                    <label for=\"pregunta_cern\"><input type=\"$tipus\" name=\"pregunta_cern\" id=\"resposta1\" id=\"resposta1\" value=\"$resposta1\">$correcte</label>
                     </div>
                     <div class=\"resposta1\">
-                        <label for=\"pregunta_cern\"><input type=\"radio\" name=\"pregunta_cern\" id=\"resposta2\" value=\"$resposta2\">$resposta2</label>
+                        <label for=\"pregunta_cern\"><input type=\"$tipus\" name=\"pregunta_cern\" id=\"resposta2\" value=\"$resposta2\">$resposta2</label>
                         </div>
                 </div>
                     <div class=\"respostesabaix\">
                         <div class=\"resposta1\">
-                            <label><input type=\"radio\" name=\"pregunta_cern\" value=\"$resposta3\" id=\"resposta3\">$resposta3</label>
+                            <label><input type=\"$tipus\" name=\"pregunta_cern\" value=\"$resposta3\" id=\"resposta3\">$resposta3</label>
                             </div>
                             <div class=\"resposta1\">
-                                <label><input type=\"radio\" name=\"pregunta_cern\"value=\"$resposta4\" id=\"resposta4\">$resposta4</label>
+                                <label><input type=\"$tipus\" name=\"pregunta_cern\"value=\"$resposta4\" id=\"resposta4\">$resposta4</label>
                                 </div>
                             <div class=\"ocult\">
                             <input type=\"hidden\" class=\"form\" id=\"numanterior\" aria-describedby=\"numanterior\" name=\"numanterior\" value=\"$numanterior\">
                             <input type=\"hidden\" class=\"form\" id=\"correcte\" aria-describedby=\"correcte\" name=\"correcte\" value=\"$correcte\">
+                            <input type=\"hidden\" class=\"form\" id=\"correcte2\" aria-describedby=\"correcte2\" name=\"correcte2\" value=\"$correcte2\">
+                            <input type=\"hidden\" class=\"form\" id=\"panteriors\" aria-describedby=\"panteriors\" name=\"panteriors\" value=\"$panteriors\">
+                            <input type=\"hidden\" class=\"form\" id=\"pactual\" aria-describedby=\"pactual\" name=\"pactual\" value=\"$pactual\">
                             </div>
                             </div>
                     <input type=\"submit\" value=\"Next >>\" class=\"enviar\" id=\"enviar\"/>
@@ -127,28 +163,47 @@
         ?>
 
         <script>
-            			document.getElementById('enviar').addEventListener('click', function() {
-                            let resposta1 = document.getElementById('resposta1')
-                            let resposta2 = document.getElementById('resposta2')
-                            let resposta3 = document.getElementById('resposta3')
-                            let resposta4 = document.getElementById('resposta4')
-                            let correcte = document.getElementById('correcte')
-                            let numanterior = document.getElementById('numanterior')
-                            if(resposta1.checked) {
 
-                                if (resposta1.value == correcte.value) {
-                                    alert("Resposta correcte")
-                                    // Sumar a numanterior.value +1
+            let panteriors = document.getElementById("panteriors") 
+            let pactual = document.getElementById("pactual");
 
-                                   
+            console.log(panteriors.value)
+            
+            document.getElementById('enviar').addEventListener('click', function() {
+            let resposta1 = document.getElementById('resposta1');
+            let resposta2 = document.getElementById('resposta2');
+            let resposta3 = document.getElementById('resposta3');
+            let resposta4 = document.getElementById('resposta4');
+            let correcte = document.getElementById('correcte');
+            let numanterior = document.getElementById('numanterior');
+            let correcte2 = document.getElementById('correcte2');
 
-                                }
+            if (correcte2.value == "false") {
+                if (resposta1.checked) {
+                    if (resposta1.value == correcte.value) {
+                        let num = parseInt(numanterior.value);
+                        if (isNaN(num)) {
+                            num = 0; // Si es NaN, se iguala a 0
+                        }
+                        numanterior.value = num + 1;
+                        // Sumar 1 a numanterior.value
+                    }
+                }
+            } else {
+                if (resposta1.checked && resposta2.checked) { // Usa && en lugar de 'and'
+                    if (resposta1.value == correcte.value && resposta2.value == correcte2.value) {
+                        let num = parseInt(numanterior.value);
+                        if (isNaN(num)) {
+                            num = 0; // Si es NaN, se iguala a 0
+                        }
+                        numanterior.value = num + 1;
+                        // Sumar 1 a numanterior.value
+                    }
+                }
+            }
+        });
 
-                            }
 
-
-
-                        })
 
         </script>
         
